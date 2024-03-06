871e2839cc162478932a291dd72243166bce4d5d
片倉秀登 <katakurashuto415@KatakuraShutoMacBook-Air.local>
2024-03-07 00:53:44 +0900

    こっちもできた！

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import getCurrentUser from './actions/getCurrentUser'
+import Timecard from './components/timecard/Timecard'
 
 export default async function Home() {
 
   const currentUser = await getCurrentUser()
 
   return (
-    <div className="text-center">{currentUser ? <div>認証中</div> : <div>未認証</div>}</div>
+    <div className="text-center">{currentUser ? <div>
+                                                  <div>認証中</div>
+                                                  <Timecard />
+                                                </div> 
+                                                : <div>未認証</div>}</div>
   )
 }
